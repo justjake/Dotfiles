696c0eca0d93c62dd321a827dc8cd73523976ab2
Jake Teton-Landis <jake.teton.landis@berkeley.edu>
2011-04-24 14:43:46 -0700

    Update will glob invisibles and not throw errors

--- .shell/update.sh
+++ .shell/update.sh
@@ -6,7 +6,9 @@ TARBALL="https://github.com/justjake/Dotfiles/tarball/master"
 DIRNAME=".dotfiles"
 LOCAL="$HOME/$DIRNAME"
 
-oldpwd="$PWD"
+# include hidden files
+shopt -s nullglob
+shopt -s dotglob 
 
 #test for git
 if [ ! -z "$(which git)" ]; then
@@ -27,8 +29,8 @@ function updateDots {
 		tar -zxf justjake-Dotfiles-*.tar.gz
 		rm justjake-Dotfiles-*.tar.gz
 		mv justjake-Dotfiles-* UpdateSource
-		cd UpdateSource
 		
+		cd UpdateSource
 		shopt -s nullglob
 		shopt -s dotglob # To include hidden files
 		for file in $(find * -type f)
@@ -43,14 +45,15 @@ function downloadDots {
 	if [ ! -z "$USEGIT" ]; then
 		git clone "https://justjake@github.com/justjake/Dotfiles.git" "$LOCAL"
 	else
-		mkdir "$LOCAL"
-		cd "$LOCAL"
-		wget -O tarball.tar.gz --no-check-certificate "$TARBALL"
-		tar -zxf tarball.tar.gz
-		rm tarball.tar.gz
-		mv * new
-		mv new/* ./
-		rm -r new
+		mkdir "$LOCAL"; cd "$LOCAL"
+		
+		wget --no-check-certificate "$TARBALL"
+		tar -zxf justjake-Dotfiles-*.tar.gz
+		rm justjake-Dotfiles-*.tar.gz
+		
+		mv justjake-Dotfiles-*/* ./
+		
+		rm -r justjake-Dotfiles-*
 	fi
 }
 
@@ -79,9 +82,12 @@ shopt -s nullglob
 shopt -s dotglob # To include hidden files
 for file in *
 do
-	cd $HOME
-	echo "Linking $file"
-	
-	ln -s "$DIRNAME"/"$file" "$file"
+	cd "$HOME"
+	if [[ -a "$DIRNAME"/"$file" ]]; then
+		echo "$file exists; skipping."
+	else
+		echo "Linking $file"
+		ln -s "$DIRNAME"/"$file" "$file"
+	fi
 	cd "$LOCAL"
 done
