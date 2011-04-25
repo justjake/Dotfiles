15055d52ccee5e8be2dacb75d25564a1804f404f
Jake Teton-Landis <jake.teton.landis@berkeley.edu>
2011-04-25 01:41:18 -0700

    push script for better tar management

---
+++ .shell/meta/push.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Use to push so I can have my pre-push hook.
+# compress using tar so we can upload via FTP to jake.teton-landis.org
+
+# Paths
+REPO="git://github.com/justjake/Dotfiles.git"
+TARBALL="https://github.com/justjake/Dotfiles/tarball/master"
+DIRNAME=".dotfiles"
+
+LOCAL="$HOME/$DIRNAME"
+SERVER="ftp.teton-landis.org"
+REMOTE="public_html/jake/repo/dotfiles"
+
+# include hidden files
+shopt -s nullglob
+shopt -s dotglob
+
+
+function push {
+	cd $LOCAL
+	git push
+}
+
+function makeTarball {
+	tarOptions="-cj --exclude '.git' --exclude '.gitignore' --exclude '.git'"
+	cd $LOCAL
+	tar $tarOptions * > $1
+}
+
+function upload {
+	curlArgs="--verbose --user jake@teton-landis.org --upload-file $1"
+	curl $curlArgs ftp://$SERVER/$REMOTE/$1
+}
+
+makeTarball master.tar.gz
+upload master.tar.gz
+rm master.tar.gz
+push
+
+exit 0
