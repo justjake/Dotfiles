ca8b5d2e45b99445b2ff26204c9c9e6c4d526ab8
Jake Teton-Landis <just.1.jake@gmail.com>
2013-05-25 15:30:43 -0700

    go env and gnome terminal workaround for colors

--- zsh/hosts/all-office-workstations.zsh
+++ zsh/hosts/all-office-workstations.zsh
@@ -2,8 +2,21 @@
 PREFIX="$HOME/prefixes/workstation"
 add-bundle-to-path "$PREFIX"
 
+# Go
+export GOROOT="$PREFIX/go"
+export GOPATH="$PREFIX/gopath"
+export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+
+# Plan9 from User Space
 PLAN9="$PREFIX/usr/local/plan9port"
 export PLAN9
 PATH="$PATH:$PLAN9/bin"
 export PATH
 
+# Aliases
+alias tbird="thunderbird & disown"
+
+# deal with Gnome Terminal being silly
+if [[ $COLORTERM == "gnome-terminal" ]] ; then
+    export TERM="xterm-256color"
+fi
