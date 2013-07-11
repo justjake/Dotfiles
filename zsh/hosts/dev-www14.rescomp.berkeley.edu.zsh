17b9c77191fc1f7ae537713a2a278b0472e3d798
Jake Teton-Landis <just.1.jake@gmail.com>
2013-07-11 14:34:12 -0700

    lotsa settings

--- zsh/hosts/dev-www14.rescomp.berkeley.edu.zsh
+++ zsh/hosts/dev-www14.rescomp.berkeley.edu.zsh
@@ -2,3 +2,23 @@
 export WORKON_HOME="$HOME/virtualenvs"
 source $(which virtualenvwrapper.sh)
 
+# Golang
+export GOROOT="$HOME/go"
+export GOPATH="$HOME/gopath"
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+eval "$(go env)"
+
+# personal preference
+PROJECT="$HOME/gopath/src/github.com/justjake/mail"
+
+### Prefix!
+PREFIX="$HOME/prefix"
+# bundle-dir "$PREFIX/bundles"
+# bin stuff - front of path
+export PATH="$PREFIX/bin:$PATH"
+# lib stuff
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+# rubygems
+export GEM_HOME="$PREFIX/rubygems"
+PATH="$GEM_HOME/bin:$PATH"
