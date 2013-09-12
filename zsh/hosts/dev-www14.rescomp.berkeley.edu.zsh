af26f4475718f958f9bfcba33403b7cea6805f68
Jake Teton-Landis <just.1.jake@gmail.com>
2013-09-11 17:00:10 -0700

    special snowflake devbox

--- zsh/hosts/dev-www14.rescomp.berkeley.edu.zsh
+++ zsh/hosts/dev-www14.rescomp.berkeley.edu.zsh
@@ -9,7 +9,8 @@ export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 eval "$(go env)"
 
 # personal preference
-PROJECT="$HOME/gopath/src/github.com/justjake/mail"
+# PROJECT="$HOME/gopath/src/github.com/justjake/mail"
+PROJECT="/usr/code/jitl/"
 
 ### Prefix!
 PREFIX="$HOME/prefix"
@@ -20,5 +21,13 @@ export PATH="$PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 # rubygems
-export GEM_HOME="$PREFIX/rubygems"
-PATH="$GEM_HOME/bin:$PATH"
+# export GEM_HOME="$PREFIX/rubygems"
+# PATH="$GEM_HOME/bin:$PATH"
+
+### rbenv
+export RBENV_ROOT="$HOME/prefixes/rhel-headless/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+
+
+which bug-helper.sh no-output && source "$(which bug-helper.sh)"
