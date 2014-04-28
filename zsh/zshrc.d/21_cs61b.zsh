49a914c21788ab0d5dde6cdbfbc4c8feb3b61be8
Jake Teton-Landis <just.1.jake@gmail.com>
2014-04-28 16:27:28 -0700

    config file for all hive machines

--- zsh/zshrc.d/21_cs61b.zsh
+++ zsh/zshrc.d/21_cs61b.zsh
@@ -3,3 +3,14 @@ export CS61B="$HOME/src/cs61b"
 61b-note () {
     $EDITOR "${CS61B}/lectures/lecture_$(date --rfc-3339=date).java"
 }
+
+if [[ $(hostname) = hive*.CS.Berkeley.EDU ]]; then
+    source "${ZSH_FILES}/hosts/all-hives.CS.Berkeley.EDU.zsh"
+fi
+
+if [[ ${USER} = cs* ]]; then 
+    # the following command causes errors :(
+    # you've tried it before :O
+    #
+    # source "$HOME/.bash_profile"
+fi
