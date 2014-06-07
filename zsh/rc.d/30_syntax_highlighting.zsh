82e75556ebdd8462532ba2936a5ff3362576dbb1
Jake Teton-Landis <just.1.jake@gmail.com>
2014-06-07 00:41:56 -0700

    fix plugin: only activate when downloaded

--- zsh/rc.d/30_syntax_highlighting.zsh
+++ zsh/rc.d/30_syntax_highlighting.zsh
@@ -1,10 +1,12 @@
 # if syntax highlighting is installed, use it
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 local plugin="$ZSH_FILES/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 if [[ -f "$plugin" ]] ; then
+    ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
     source "$plugin"
+
+    ZSH_HIGHLIGHT_STYLES[root]='bg=red'
+    ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
 fi
 
-ZSH_HIGHLIGHT_STYLES[root]='bg=red'
-ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
