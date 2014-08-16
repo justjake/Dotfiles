365347901e51cc01a38e9c6cb6b7467e4314088f
Jake Teton-Landis <just.1.jake@gmail.com>
2014-08-16 12:59:04 -0700

    sh support for bundle stuff

--- zsh/rc.d/02_bundles.zsh
+++ zsh/rc.d/02_bundles.zsh
@@ -3,7 +3,7 @@
 # a bundle is a UNIX prefix
 # this prepends the bundle's directories to your various tool
 # load paths, so they take precedence over system versions
-add-bundle-to-path () {
+add_bundle_to_path () {
     local bundle="$1"
     [ -d "$bundle/bin" ] && export PATH="$bundle/bin:$PATH"
     [ -d "$bundle/share/man" ] && export MANPATH="$bundle/share/man:$MANPATH"
@@ -13,23 +13,29 @@ add-bundle-to-path () {
     [ -d "$bundle/include" ] && export C_INCLUDE_PATH="$bundle/include:$C_INCLUDE_PATH"
 
     # add sub folders like "usr"
-    [ -d "$bundle/usr" ] && add-bundle-to-path "$bundle/usr"
+    [ -d "$bundle/usr" ] && add_bundle_to_path "$bundle/usr"
 }
 
 # I like to use ~/bundles to store my personal software in
 # its own directory. This is a good place for java, intellij,
 # my custom tmux, etc
-bundle-dir () {
+bundle_dir () {
     local BUNDLES="$1"
     local bundle
     if [ -d "$BUNDLES" ]; then
         for bundle in "$BUNDLES"/*; do
-            add-bundle-to-path $bundle
+            add_bundle_to_path $bundle
         done
     else
         echo "$BUNDLES does not exist"
     fi
 }
 
+# wat
+if [ $SHELL = /bin/zsh ] || [ $SHELL = /bin/bash ] ; then
+    alias 'add-bundle-to-path'=add_bundle_to_path
+    alias 'bundle-dir'=bundle_dir
+fi
+
 DEFAULT_BUNDLES="$HOME/bundles"
-[ -d "$DEFAULT_BUNDLES" ] && bundle-dir "$DEFAULT_BUNDLES"
+[ -d "$DEFAULT_BUNDLES" ] && bundle_dir "$DEFAULT_BUNDLES"
