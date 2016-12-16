febe6d8441a40771a487793d2c7c129712b440b0
Jake Teton-Landis <jake.tl@airbnb.com>
2016-12-15 18:49:49 -0800

    update airbnb config with some useful aliases

--- zsh/hosts/airbook.internal.zsh
+++ zsh/hosts/airbook.internal.zsh
@@ -1,13 +1,17 @@
-add_bundle_to_path /usr/local
 eval "$(rbenv init -)"
+
+export GOPATH="$HOME/src/golang"
+export PATH="$PATH:${GOPATH//://bin:}/bin"
+export PATH="/usr/local/sbin:$PATH"
 
 # swag
 # https://github.com/micha/resty
-source "$HOME/bin/resty"
+# source "$HOME/bin/resty" # <-- was causing errors
 
 alias a='cd ~/src/airbnb; ls'
 alias p='cd ~/src/phoenix; ls'
 alias c='cd ~/src/chef; ls'
+alias rc='cd ~/src/codenames/redux-codenames; ls'
 # alias n='nesh'
 alias d='cd ~/src/deployboard; ls'
 alias vboxrestart='sudo /Library/StartupItems/VirtualBox/VirtualBox restart'
@@ -35,3 +39,59 @@ export DYLD_LIBRARY_PATH=/Users/jake_teton-landis/bundles/torch/install/lib:$DYL
 # CUDA, a thing for things
 export PATH=/Developer/NVIDIA/CUDA-7.0/bin:$PATH
 export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-7.0/lib:$DYLD_LIBRARY_PATH
+
+# query optica for role
+optica-fetch () {
+  curl -s http://optica.d.musta.ch/\?role\=$@
+}
+
+optica-fetch-exact () {
+  optica-fetch '^'$@'$'
+}
+
+# stream in an Optica JSON, get out a whitespace ceperated list of
+# hostname role branch
+optica-to-table () {
+  jq -r '.nodes[] | [.hostname, .role, .branch] | @tsv' | column -t
+}
+
+nodes-like () {
+  optica-fetch $@ | optica-to-table
+}
+
+nodes () {
+  optica-fetch-exact $@ | optica-to-table
+}
+
+# expects STDIN containing optica talbe info
+ssh-first () {
+  local first="$(head -n 1)"
+  ssh-node ${=first}
+}
+
+ssh-node () {
+  local node="$1"
+  local role="$2"
+  local branch="$3"
+
+  local host="${node}.inst.aws.airbnb.com"
+
+  echo "id:     $node"
+  echo "role:   $role"
+  echo "branch: $branch"
+  echo ">> ssh ${host}"
+  echo "$host" | xargs -o ssh
+}
+
+ssh-to () {
+  nodes $@ | ssh-first
+}
+
+# nvm use v4 --delete-prefix
+
+export EDITOR="nvim"
+alias vim=nvim
+alias vi=/usr/local/bin/vim
+export I_AM_AN_AIRLAB_DEVELOPER="haha hells yes"
+
+export NVIM_LISTEN_ADDRESS="$HOME/.vim/sockets/vim-zsh-$$.sock"
