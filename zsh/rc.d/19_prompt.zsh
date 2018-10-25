d5012852a3a58a9447a8e8d0d6d8b04a594f68d4
Jake Teton-Landis <jake.teton-landis@airbnb.com>
2018-10-24 19:56:14 -0700

    zsh: two-line prompt

--- zsh/rc.d/19_prompt.zsh
+++ zsh/rc.d/19_prompt.zsh
@@ -1,8 +1,8 @@
 ### VCS module required
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git hg
-zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats       '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
+zstyle ':vcs_info:*' actionformats '%F{0}[%f%s%F{5}:%F{3}%F{5}%F{2}%b%F{3}|%F{1}%a%F{0}]%f'
+zstyle ':vcs_info:*' formats       '%F{0}[%f%s%F{5}:%F{3}%F{5}%F{2}%b%F{0}]%f'
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 
 ### Colors and Prompt
@@ -16,5 +16,5 @@ for color in BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
 done
 PR_RESET_COLOR="%{$reset_color%}"
 
-export PROMPT='${PR_LIGHT_BLACK}[${PR_LIGHT_BLUE}%n${PR_LIGHT_BLACK}@${PR_RESET_COLOR}${PR_GREEN}%m${PR_LIGHT_BLACK}:${PR_LIGHT_GREEN}%2c${PR_LIGHT_BLACK}]${PR_RESET_COLOR}${PR_RED} %(!.#.$)${PR_RESET_COLOR} '
-export RPROMPT='${PR_LIGHT_BLACK}(%D{%m-%d %H:%M}) [%?] ${vcs_info_msg_0_}${PR_RESET_COLOR}' # shows exit status of previous command
+export PROMPT='${PR_LIGHT_BLACK}[${PR_LIGHT_BLUE}%n${PR_LIGHT_BLACK}@${PR_RESET_COLOR}${PR_GREEN}%m${PR_LIGHT_BLACK}:${PR_LIGHT_GREEN}%2c${PR_LIGHT_BLACK}]${PR_RESET_COLOR}${PR_LIGHT_BLACK}${vcs_info_msg_0_}${PR_RESET_COLOR} ${PR_LIGHT_BLACK}%D{%m-%d %H:%M} [%?]
+${PR_RESET_COLOR}${PR_RED}%(!.#.$)${PR_RESET_COLOR} '
