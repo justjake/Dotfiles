978905968484a6d5c9a8a3e80ee30dae6cc49539
Jake Teton-Landis <jake.teton-landis@airbnb.com>
2018-10-25 19:57:36 -0700

    more fun tmux

--- tmux/source-correct-version.sh
+++ tmux/source-correct-version.sh
@@ -4,8 +4,11 @@ verify_tmux_verision () {
   tmux_home=~/.dotfiles/tmux
   tmux_version="`tmux -V | cut -c 6-`"
 
-  if [ `echo "$tmux_version >= 2.1" | bc` -eq 1 ]; then
+  if [ `echo "$tmux_version >= 2.8" | bc` -eq 1 ]; then
     tmux source-file "$tmux_home/tmux_ge_21.conf"
+    exit 0
+  elif [ `echo "$tmux_version >= 2.1" | bc` -eq 1 ]; then
+    tmux source-file "$tmux_home/tmux_21_28.conf"
     exit 0
   else
     tmux source-file "$tmux_home/tmux_lt_21.conf"
