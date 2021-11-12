f3fc9b87e35b3ac32e7918d6f39628cfea5af03d
Jake Teton-Landis <just.1.jake@gmail.com>
2021-11-12 13:51:52 -0800

    unused: sysconfig.sh

--- meta/sysconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# Package listings.
-# Usage:
-#   source ~/.dotfiles/jark/sysconfig.sh
-#   sudo $PKG_INSTALL (system config here)
-
-$VERSION_CONTROLL="git mercurial"
-$VIM="vim-gnome"
-$SHELL="zsh roxterm $VIM"
