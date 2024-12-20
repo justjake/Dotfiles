c72aff3112d9d315dbb113eb07b29f4db34ae653
Jake Teton-Landis <just.1.jake@gmail.com>
2024-12-19 22:40:18 -0800

    vim navigator restored

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+vim.g.snacks_animate = false
+opt.relativenumber = false
