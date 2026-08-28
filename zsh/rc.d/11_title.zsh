c791462445bb7027f6ea6cd16255f55d05b74248
Jake Teton-Landis <just.1.jake@gmail.com>
2026-08-28 12:57:55 -0400

    Harden zsh title and scmpuff hooks

--- zsh/rc.d/11_title.zsh
+++ zsh/rc.d/11_title.zsh
@@ -1,37 +1,36 @@
 #### Window Title
 title () {
-  # escape '%' chars in $1, make nonprintables visible
-  local running_cmd="${(V)1//\%/\%\%}"
+  # Make nonprintables visible.
+  local running_cmd="${(V)1}"
 
-  # Strip newlines from command
-  running_cmd="$(print -Pn "%70>...>$running_cmd" | tr -d "\n")"
+  if (( ${#running_cmd} > 70 )); then
+    running_cmd="${running_cmd[1,67]}..."
+  fi
 
   local title="$2 $running_cmd"
-  local xtermtitle="\e]2;$title\a"
+  local xtermtitle=$'\e]2;'"$title"$'\a'
 
   case $TERM in
   screen*)
-    print -Pn "$xtermtitle" # plain xterm title
-    print -Pn "\ek$title\e\\" # screen title (in ^A")
-    print -Pn "\e_$2\e\\"   # screen location
+    print -rn -- "$xtermtitle" # plain xterm title
+    print -rn -- $'\ek'"$title"$'\e\\' # screen title (in ^A")
+    print -rn -- $'\e_'"$2"$'\e\\'   # screen location
     ;;
   xterm*|rxvt*)
-    print -Pn "$xtermtitle" # plain xterm title
+    print -rn -- "$xtermtitle" # plain xterm title
     ;;
   esac
 }
 
-TITLE_LOCATION="%~"
-
 # precmd is called just before the prompt is printed
 function jake-precmd {
-    title "zsh" "$TITLE_LOCATION"
+    title "zsh" "${PWD/#$HOME/~}"
     vcs_info
 }
 
 # preexec is called just before any command line is executed
 function jake-preexec() {
-  title "$1" "$TITLE_LOCATION"
+  title "$1" "${PWD/#$HOME/~}"
 }
 
 #### Add ZSH pre-command hook
--- zsh/rc.d/32_scmpuff.zsh
+++ zsh/rc.d/32_scmpuff.zsh
@@ -1,3 +1,34 @@
 if command-exists scmpuff; then
   eval "$(scmpuff init -s)"
+
+  scmpuff_status() {
+    local cmd_output
+    cmd_output="$(scmpuff status --filelist "$@")"
+
+    local es=$?
+    if [ $es -ne 0 ]; then
+      return $es
+    fi
+
+    local files file name line
+    local -i e=1 bytes=0 max_bytes=$((256 * 1024))
+    {
+      IFS= read -r files
+      scmpuff_clear_vars
+
+      local LC_ALL=C
+      while IFS= read -r -d $'\t' file; do
+        (( e > 999 )) && break
+        name="e$e"
+        (( bytes + ${#name} + ${#file} + 1 > max_bytes )) && break
+        export "$name=$file"
+        (( bytes += ${#name} + ${#file} + 1 ))
+        (( e++ ))
+      done < <(print -rn -- "$files"$'\t')
+
+      while IFS= read -r line; do
+        print -r -- "$line"
+      done
+    } < <(print -r -- "$cmd_output")
+  }
 fi
